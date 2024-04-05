9bbbd9ea18d417c12366e4a14efead83757a54da
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-04-05 08:59:28 -0300

    conexão entre controller e view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+
   def index
+    @var_controller_index = "Oi, eu sou uma variável declarada no controller 
+    -> welcome_controller.rb <- e instanciada com @var_controller_index no welcome.html.erb nas views
+    para que eu possa aparecer aqui para você no navegador."
   end
 end
